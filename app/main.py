47301617a6b03420f6ba2a5a798f25a0cdc60a3f
1AprilJoker <86672070+1AprilJoker@users.noreply.github.com>
2026-04-21 18:41:59 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -50,21 +50,26 @@ async def alexa(req: Request):
     # =========================
     if intent == "PlayMusicIntent":
 
-        token = get_token(session_id)
+    token = get_token(session_id)
 
-        if not token:
-            answer = "Spotify не подключен"
+    if not token:
+        answer = "Spotify не подключен"
 
+    else:
+        access_token = token["access_token"]
+
+        uri = search_track(user_text, access_token)
+
+        print("TRACK URI:", uri)
+
+        if uri:
+            status = play_track(uri, access_token)
+
+            print("PLAY STATUS:", status)
+
+            answer = "Включаю музыку"
         else:
-            access_token = token["access_token"]
-
-            uri = search_track(user_text, access_token)
-
-            if uri:
-                play_track(uri, access_token)
-                answer = "Включаю музыку"
-            else:
-                answer = "Не нашёл подходящую песню"
+            answer = "Не нашёл трек"
 
     # =========================
     # 🤖 AI MODE (DEFAULT)
